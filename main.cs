f0a229703e5e9c99b00eb705d880d138b3575beb
juliakimchung <julhkm@gmail.com>
2017-05-05 11:29:09 -0500

    basic c# calculator

---
+++ main.cs
@@ -0,0 +1,58 @@
+using System;
+namespace Calculator
+{
+	class Program
+	{
+		static void Main()
+		{
+			
+			Console.WriteLine("Select operation");
+			Console.WriteLine("1. Add");
+			Console.WriteLine("2. Subtract");
+			Console.WriteLine("3. Multiply");
+			Console.WriteLine("4. Divide");
+		  var choice = Console.ReadLine();
+		  if(choice == "1")
+		  {
+		  	Console.WriteLine("Enter first number");
+		  	var num1 = Console.ReadLine();
+		  	Console.WriteLine("Enter second number");
+		  	var num2 = Console.ReadLine();
+		  	Console.WriteLine(double.Parse(num1) + double.Parse(num2));
+
+		  }
+		  else if(choice == "2")
+		  {
+		  	Console.WriteLine("Enter first number");
+		  	var num1 = Console.ReadLine();
+		  	Console.WriteLine("Enter second number");
+		  	var num2 = Console.ReadLine();
+		  	Console.Write("Answer: ");
+		  	Console.WriteLine(double.Parse(num1) - double.Parse(num2));
+		  }
+		  else if(choice == "3")
+		  {
+		  	Console.WriteLine("Enter first number");
+		  	var num1 = Console.ReadLine();
+		  	Console.WriteLine("Enter second number");
+		  	var num2 = Console.ReadLine();
+		  	Console.Write("Answer: ");
+		  	Console.WriteLine(double.Parse(num1) * double.Parse(num2));
+		  }
+		  else if(choice == "4")
+		  {
+		  	Console.WriteLine("Enter first number");
+		  	var num1 = Console.ReadLine();
+		  	Console.WriteLine("Enter second number");
+		  	var num2 = Console.ReadLine();
+		  	Console.Write("Answer: ");
+		  	Console.WriteLine(double.Parse(num1) / double.Parse(num2));
+		  }
+		  else
+		  {
+		  	Console.WriteLine("Invalid input");
+		  }
+
+		}
+	}
+}
